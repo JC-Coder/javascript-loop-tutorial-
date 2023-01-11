43af3fd99ba0af9a378c4ea8c47c6d23157435f3
JC-Coder <josephchimezie2003@gmail.com>
2023-01-11 23:09:52 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,74 @@
+/**
+ * Javascript loop
+ */
+
+/**
+ * 1. for loop
+ * 2. while loop
+ * 3. do-while loop
+ * 4. for-in loop
+ */
+
+// i++ 
+// i += 1;
+// i + 1
+
+// i = 0;
+// i = 1;
+// i = 2;
+// i = 3;
+// i = 9;
+// i = 10;
+
+
+// for loop 
+// for(let i = 5; i <= 20; i++){
+//     console.log(i);
+// }
+
+// while loop 
+// let i = 0;
+
+// while (i < 20){
+//     i++;
+//     console.log(i);
+// }
+
+// Note : in for loop and while loop we check the condtion before the action but in do while loop we check the action before the condition
+
+// let i = 0;
+
+// do{
+//     i++;
+//     console.log(i);
+// } while (i < 20);
+
+
+// Number loop game 
+// let userInput = +prompt("enter a number");
+
+// for(let i = 0; i <= userInput; i++){
+//     if(i == 20){
+//         continue
+//     }
+
+//     console.log(i);
+
+//     if(i == 500){
+//         break;
+//     }
+// }
+
+// break - this stops a loop 
+// continue - skips a loop item 
+
+
+// guess a number game 
+// careful when uncommenting this as it is an infinite loop 
+
+// let userInput = +prompt("enter a name");
+
+// let value = 'joe'
+// for(let i = 0; value != userInput; i++){
+//     console.log(i)
+// }
